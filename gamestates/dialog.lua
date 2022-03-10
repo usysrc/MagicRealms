c2c71340688c788b1a41fb32eec963ae308f15ed
usysrc <tilmann.hars@googlemail.com>
2022-03-10 22:33:48 +0100

    dialog downarrow

--- gamestates/dialog.lua
+++ gamestates/dialog.lua
@@ -8,6 +8,8 @@ local str = require "lib.str"
 local pos, text, title
 local speed = 30
 local timer
+local blink
+local time
 
 local dialog = {}
 dialog.init = function() 
@@ -31,6 +33,7 @@ local startRevealText = function()
 end
 
 dialog.enter = function(self, prev, data)
+    time, blink = 0, 0
     pos = 0
     text = str.partition(data.text)
     title = data.title
@@ -40,20 +43,25 @@ end
 
 dialog.update = function(self, dt)
     timer:update(dt)
-    
+    time = time + dt
+    blink = math.sin(time*10)
 end
 
 dialog.draw = function(self)
+    love.graphics.setColor(1,1,1,1)
     love.graphics.draw(Image.framehori, 0, display:getHeight() - 64)
     if title then 
         love.graphics.print(title..":", 32, display:getHeight() - 52) 
     end
-    love.graphics.print(string.sub(text, 1, pos), 100, display:getHeight() - 52) 
+    love.graphics.print(string.sub(text, 1, pos), 100, display:getHeight() - 52)
+    love.graphics.setColor(1,1,1, blink)
+    love.graphics.draw(Image.downarrow, display:getWidth()/2, display:getHeight() - 16)
+    love.graphics.setColor(1,1,1,1)
 end
 
 dialog.keypressed = function(self, k)
     if k == 'return' or k == 'space' then
-        if pos ~= #text then
+        if pos < #text then
             timer:clear()
             pos = #text
         else
