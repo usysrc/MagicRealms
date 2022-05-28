c200ef9fddaebfa45ec8e364403f64a2e45c79c5
usysrc <usysrc@usysrc.de>
2022-05-28 09:27:56 +0200

    added escape to death scene

--- gamestates/died.lua
+++ gamestates/died.lua
@@ -1,3 +1,5 @@
+local Gamestate = require "lib.hump.gamestate"
+-- local game = require "gamestates.game"
 
 
 local died = {}
@@ -5,6 +7,13 @@ local died = {}
 died.draw = function()
     love.graphics.clear()
     love.graphics.print("YOU DIED", 250, 150)
+end
+
+died.keypressed = function(self, key)
+    if key == "escape" then
+        -- Gamestate.switch(game)
+        love.event.quit()
+    end
 end
 
 return died
